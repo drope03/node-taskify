a3bf8520ee3af403a93e27a126e93fc2d7a8c1c7
Pedro Neto <netopedroo117@gmail.com>
2025-05-16 23:06:58 -0300

    arquivo de roas base inicial

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { Database } from "./database.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "",
+    path: "",
+    handler: (req, res) => {},
+  },
+];
