e7ad7780f9ee9a66469b444587b2261159125dab
Pedro Neto <netopedroo117@gmail.com>
2025-06-10 17:37:52 -0300

    alternado linha de comando no package

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {
-  return rex.end("Olá mundo!");
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await json(req, res);
+
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
+
+  if (route) {
+    const 
+  }
 });
 
 server.listen(3333);
